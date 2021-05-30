62226257fb0aa25e65139cf1b10a74ea88281938
lilimin <290059742@qq.com>
2021-05-30 11:54:41 +0800

    完善实现

--- src/main/java/com/javashitang/server/ServerSpiDemo.java
+++ src/main/java/com/javashitang/server/ServerSpiDemo.java
@@ -1,5 +1,7 @@
 package com.javashitang.server;
 
+import com.javashitang.util.SpiUtil;
+
 import java.util.ServiceLoader;
 import java.util.stream.StreamSupport;
 
@@ -13,9 +15,12 @@ public class ServerSpiDemo {
      * 会先加载自己包的实现，再加载其他包的实现
      */
     public static void main(String[] args) {
-        RemoteServer rpcAccessPoint = StreamSupport.
+        RemoteServer remoteServer = StreamSupport.
                 stream(ServiceLoader.load(RemoteServer.class).spliterator(), false)
                 .findFirst().orElse(null);
-        System.out.println(rpcAccessPoint);
+        System.out.println(remoteServer);
+
+        RemoteServer server = SpiUtil.load(RemoteServer.class);
+        System.out.println(server);
     }
 }
---
+++ src/main/java/com/javashitang/util/SpiUtil.java
@@ -0,0 +1,18 @@
+package com.javashitang.util;
+
+import java.util.ServiceLoader;
+import java.util.stream.StreamSupport;
+
+/**
+ * @author lilimin
+ * @since 2021-05-30
+ */
+public class SpiUtil {
+
+    public synchronized static <S> S load(Class<S> service) {
+        return StreamSupport.
+                stream(ServiceLoader.load(service).spliterator(), false)
+                .findFirst().orElse(null);
+    }
+}
+
